d0a64939a2d48a92ecb62ef55fd756fbb1cc2b84
mthura2214 <mthuthuzeli.magidigidi@capaciti.org.za>
2024-11-20 08:47:33 +0200

    my weather app first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Weather from './components/Weather';
+import './index.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="weather">
+        <Weather />
+      </div>
+    </div>
+  );
+}
+
+export default App;
